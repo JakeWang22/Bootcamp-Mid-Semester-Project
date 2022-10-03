596ffbbdc07c3ff9560cae899311e8b4cb4cb869
Jake <jwang3335@gatech.edu>
2022-10-03 15:14:58 -0400

    fix errors

--- start.js
+++ start.js
@@ -1,9 +1,42 @@
-import fetch from "node-fetch";
 
-const name = fetch("https://pokeapi.co/api/v2/pokemon/1")
-    .then((response) => response.json())
-    .then((data) => console.log(data.name));
+/*
+let next = document.getElementById("next");
+let index = 0;
+next.addEventListener("click", () => {
+    counter++;
+})
 
+let print = document.getElementById("print");
+print.addEventListener("click", () => {
+    console.log(counter);
+});
+*/
+/*const redText = document.getElementById("to-have-red-text");
+
+redText.style.color = "red";
+
+redText.addEventListener("click", (e) => {
+  redText.textContent = "Bog";
+});
+
+
+
+const button = document.getElementById("next");
+button.addEventListener("click", function handleClick() { 
+    button.textContent = "Button Clicked";
+})
+*/
+const button = document.getElementById("button");
+let index = 1;
+button.addEventListener("click", () => {
+    if (index!=0)
+    fetch(`https://pokeapi.co/api/v2/pokemon/${index}`)
+        .then((response) => response.json())
+        .then((data) => {
+
+        });
+    index++;
+});
 const order = fetch("https://pokeapi.co/api/v2/pokemon/1")
     .then((response) => response.json())
     .then((data) => console.log(data.order));
