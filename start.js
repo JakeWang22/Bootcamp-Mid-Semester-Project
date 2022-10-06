939d3c93420d3d7a5234d84d2d68b03c4eee2fdb
Jake <jwang3335@gatech.edu>
2022-10-06 00:44:39 -0400

    data for stats

--- start.js
+++ start.js
@@ -26,16 +26,50 @@ button.addEventListener("click", function handleClick() {
     button.textContent = "Button Clicked";
 })
 */
-const button = document.getElementById("button");
+const name = document.getElementById("name");
+const height = document.getElementById("height");
+const weight = document.getElementById("weight");
+const hp = document.getElementById("hp");
+const attack = document.getElementById("attack");
+const defense = document.getElementById("defense");
+const specialAttack = document.getElementById("special-attack");
+const specialDefense = document.getElementById("special-defense");
+const speed = document.getElementById("speed");
+const buttonNext = document.getElementById("next");
 let index = 1;
-button.addEventListener("click", () => {
+buttonNext.addEventListener("click", () => {
     if (index!=0)
     fetch(`https://pokeapi.co/api/v2/pokemon/${index}`)
         .then((response) => response.json())
         .then((data) => {
-
+            name.textContent = data.name;
+            height.textContent = "height: " + data.height;
+            weight.textContent = "weight: " + data.weight;
+            hp.textContent = "hp: " + data.stats[0].base_stat;
+            attack.textContent = "attack: " + data.stats[1].base_stat;
+            defense.textContent = "defense: " + data.stats[2].base_stat;
+            specialAttack.textContent = "special-attack: " + data.stats[3].base_stat;
+            specialDefense.textContent = "special-defense: " + data.stats[4].base_stat;
+            speed.textContent = "speed: " + data.stats[5].base_stat;
         });
     index++;
+});
+const buttonPrev = document.getElementById("prev");
+buttonPrev.addEventListener("click", () => {
+    if (index!=0)
+    fetch(`https://pokeapi.co/api/v2/pokemon/${index}`)
+        .then((response) => response.json())
+        .then((data) => {
+            name.textContent = data.name;
+            height.textContent = "height: " + data.height;
+            weight.textContent = "weight: " + data.weight;
+            hp.textContent = "hp: " + data.hp;
+            attack.textContent = "attack: " + data.attack;
+            defense.textContent = "defense: " + data.defense;
+            specialAttack.textContent = "special-attack: " + data.specialAttack;
+            specialDefense.textContent = "special-defense: " + data.specialDefense;
+        });
+    index--;
 });
 const order = fetch("https://pokeapi.co/api/v2/pokemon/1")
     .then((response) => response.json())
